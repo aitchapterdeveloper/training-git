e2581e64c25d1ec226d99d743e9ad35c0df5fe09
Adicipta <Adicipta@ACETRAINING-2.ad-ins.com>
2026-08-07 10:04:11 +0700

    add function hello

--- GitTrainingDemo/Program.cs
+++ GitTrainingDemo/Program.cs
@@ -5,3 +5,6 @@ Console.WriteLine(userService.Login("admin", "1234"));
 
 var orderService = new OrderService();
 Console.WriteLine(orderService.CalculateTotal(100000));
+
+var SapaService = new SapaService();
+Console.WriteLine(SapaService.SapaNama("Rizal"));
