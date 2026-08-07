f2d817e02c42c77c471d5ab55a0567e11ff03b01
Seto Apriawan <setoapriawan69@gmail.com>
2026-08-07 13:19:04 +0700

    Fix Sapa and Order Function on OrderService.cs

--- GitTrainingDemo/Services/OrderService.cs
+++ GitTrainingDemo/Services/OrderService.cs
@@ -4,14 +4,14 @@ namespace GitTrainingDemo.Services
     {
         public decimal CalculateTotal(decimal price)
         {
-            double discount = 0.15;
+            decimal discount = 0.15m;
             return price*(1-discount);
         }
        
     }
     public class SapaService
     {
-         public string SapaNama(name string)
+         public string SapaNama(string name )
         {
             return "selamat datang" + name;
         }
