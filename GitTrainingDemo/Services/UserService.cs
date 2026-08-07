b375032e80de3674e974ca58f3dc3fc98161850b
Adicipta <Adicipta@ACETRAINING-12.ad-ins.com>
2026-08-07 10:07:25 +0700

    test: git commit testing

--- GitTrainingDemo/Services/UserService.cs
+++ GitTrainingDemo/Services/UserService.cs
@@ -10,7 +10,7 @@ namespace GitTrainingDemo.Services
             }
             else if (user == "admin")
             {
-                return "Password salah";
+                return "Password salah"; //No password
             }
             else
             {
