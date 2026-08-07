0a08b747c788197d2cbe902861f6363b8c8fbc64
PPebriano <putra.pbriano@gmai.com>
2026-08-07 11:07:25 +0700

    Merubah di user service

--- GitTrainingDemo/Services/UserService.cs
+++ GitTrainingDemo/Services/UserService.cs
@@ -20,3 +20,29 @@ namespace GitTrainingDemo.Services
         }
     }
 }
+
+
+/*
+- PRAKTEK 1 (LOCAL REPO)
+git add
+git commit -m ""
+git log -- oneline
+git remote -v
+
+- PRAKTEK 2 (BRANCHING)
+git branch
+git branch <nama_branch>
+git checkout <nama_branch>
+git checkout -b <nama_branch>
+
+- PRAKTEK 3 (MERGE)
+berada di branch tujuan
+git merge <nama_branch_asal>
+
+- PRAKTEK 4 (MERGE CONFLICT)
+git status (ditandai both modified)
+buka edit file
+hapus marker di dalam file (<<<<, =====, >>>>)
+git add
+gir commit
+ */
