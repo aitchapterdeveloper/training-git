1259cc7a1c23317d34e4f3a048b4f3b740b5fc9f
Naufal <developer.naufal@gmail.com>
2026-08-07 11:07:53 +0700

    feat: Add comment to userservice.cs

--- GitTrainingDemo/Services/UserService.cs
+++ GitTrainingDemo/Services/UserService.cs
@@ -6,7 +6,7 @@ namespace GitTrainingDemo.Services
         {
             if (user == "admin" && pass == "1234")
             {
-                return "Login berhasil";
+                return "Login berhasil"; //yess
             }
             else if (user == "admin")
             {
