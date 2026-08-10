dd82a89be24764f4b9634a8a68ba12c583608455
ferdi <ferdiahmdi@gmail.com>
2026-08-10 17:08:05 +0700

    solid: <<

--- GitTrainingDemo/Services/PaymentService.cs
+++ GitTrainingDemo/Services/PaymentService.cs
@@ -3,35 +3,105 @@ using System.Collections.Generic;
 
 namespace GitTrainingDemo.Services
 {
-    public class PaymentService
+    public interface IProcessPaymentType
     {
-        public void ProcessPayment(string method, double amount, string customerEmail)
+        void Pay(double amount);
+        double GetAdminFee(double amount);
+    }
+    public interface IProcessPaymentFixedFeeType
+    {
+        void Pay(double amount);
+        double GetAdminFee();
+    }
+    public interface IReceiptSender
+    {
+        void SendReceipt(double amount, string customerEmail);
+    }
+    public interface IProcessPayment
+    {
+        void Process(IProcessPaymentType processPaymentType, string customerEmail, double amount);
+    }
+
+    public class CreditCardPayment : IProcessPaymentType 
+    {
+        public double GetAdminFee(double amount)
+        {
+            double ADMIN_FEE = 0.03;
+
+            return amount * ADMIN_FEE;
+        }
+
+        public void Pay (double amount)
+        {
+            double total = 0;
+            
+            total = amount + GetAdminFee(amount);
+
+            Console.WriteLine($"[CC] Charging Rp{total} to credit card");
+        }
+    }
+
+    public class BankTransferPayment : IProcessPaymentFixedFeeType
+    {
+        public double GetAdminFee()
+        {
+            double ADMIN_FEE_FIXED = 5000;
+            return ADMIN_FEE_FIXED;
+        }
+
+        public void Pay (double amount)
+        {
+            double total = 0;
+
+            total = amount + GetAdminFee();
+
+            Console.WriteLine($"[TF] Transferring Rp{total} via bank");
+        }
+    }
+
+    public class EWalletPayment : IProcessPaymentType
+    {
+        public double GetAdminFee(double amount)
+        {
+            double ADMIN_FEE = 0.01;
+
+            return amount * ADMIN_FEE;
+        }
+
+        public void Pay (double amount)
+        {
+            double total = 0;
+           
+            total = amount + GetAdminFee(amount);
+
+            Console.WriteLine($"[EW] Deducting Rp{total} from e-wallet");   
+        }
+    }
+
+    public class EmailReceiptSender : IReceiptSender
+    {
+        public void SendReceipt(double amount, string customerEmail) => Console.WriteLine($"[EMAIL] Receipt sent to {customerEmail}: Rp{amount}");
+    }
+
+    public class ProcessPayment : IProcessPayment
+    {
+        private readonly IReceiptSender _receiptSender;
+
+        public ProcessPayment (IReceiptSender receiptSender)
+        {
+            _receiptSender = receiptSender;
+        }
+
+        public void Process(IProcessPaymentType processPaymentType, string  customerEmail, double amount)
         {
             if (amount <= 0)
             {
                 Console.WriteLine("Amount harus lebih dari 0!");
                 return;
             }
-    }
 
-            double adminFee = 0;
-            if (method == "CreditCard")
-                adminFee = amount * 0.03;
-            else if (method == "BankTransfer")
-                adminFee = 5000;
-            else if (method == "EWallet")
-                adminFee = amount * 0.01;
-
-            double total = amount + adminFee;
-
-            if (method == "CreditCard")
-                Console.WriteLine($"[CC] Charging Rp{total} to credit card");
-            else if (method == "BankTransfer")
-                Console.WriteLine($"[TF] Transferring Rp{total} via bank");
-            else if (method == "EWallet")
-                Console.WriteLine($"[EW] Deducting Rp{total} from e-wallet");
-
-            Console.WriteLine($"[EMAIL] Receipt sent to {customerEmail}: Rp{total}");
+            processPaymentType.Pay(amount);
         }
     }
+
 }
